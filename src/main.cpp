9a93c1c5a666b2ecbd12cf1fd76ed879be7883ce
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2022-03-26 02:27:02 +0100

    Led Builtin heartbeat
    # Liste des mots clés de titre de commit (version courte -> résultat long)
    # r <num> -> Récupération du Redmine (Redmine #<num> : <titre_du_redmine>)
    # t <titre> -> Technique : <titre>
    # f <titre> -> Fix : <titre>
    # ty <titre> -> Typo : <titre>
    # d <titre> -> Doc : <titre>
    # s <titre> -> Sécurité : <titre>
    # u <titre> -> UI : <titre>
    # c <titre> -> CI : <titre>
    # dr <titre> -> Draft: <titre>
    # [skip ci] en début de titre de commit permet de ne pas déclencher de build
    #
    # Chaque ligne de description ajouté sera préfixé automatiquement d'une puce.
    #
    # Pour la saisie de temps automatique lors de l'écriture d'un commit le format est le suivant :
    # /spend <options>
    #
    # NB : Plusieurs /spend peuvent être défini sur un même commit
    #
    # Les options sont :
    # - Temps en heure (ie 0.25). Obligatoire
    # - Numéro de redmine. Facultatif si le commit porte sur un redmine avec la syntaxe courte ou longue.
    # - Catégorie de l'imputation. Facultatif. Récupération par les premières lettres en format court, si non défini Développement sera utilisé
    # - Date de l'imputation au format ISO8601 (ie 2021-02-23)
    #
    # NB : L'imputation n'est pas bloquante pour le message de commit si une erreur survient.

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,7 @@ void setup()
 
   analogReference(EXTERNAL);
   pinMode(A0, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
 #if defined(SCREEN)
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
@@ -143,6 +144,11 @@ void loop() {
 
   FastLED.show();
   FastLED.delay(2);
+
+  EVERY_N_SECONDS(1) {
+    digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) == LOW ? HIGH : LOW);
+  }
+}
 
 void i2cRequest() {
   Wire.write(carreFouille);
